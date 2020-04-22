a54963c81379ddbe8273a550d57fc497e24ff077
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-22 09:45:58 +0700

    Show instrument

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -9,7 +9,7 @@ class Instrument < ApplicationRecord
     is the maximum allow" }
   validates :title, length: { maximum: 140, too_long: "%{count} characters is 
     the maximum allow" }
-  validates :price, numericality: { only_integer: true }, length: { maximum: 10 }
+  validates :price, length: { maximum: 10 }
 
   BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS }
   FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
