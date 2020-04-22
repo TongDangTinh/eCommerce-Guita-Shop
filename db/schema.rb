883e3ff6953408414039900604b28f2d8d5fa23d
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-22 14:14:07 +0700

    Cart scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_095738) do
+ActiveRecord::Schema.define(version: 2020_04_22_071155) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
