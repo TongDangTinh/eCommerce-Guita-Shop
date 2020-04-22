aeb4a374c291261dbd06381285a659fa0b3a4caa
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-22 08:58:53 +0700

    index instruments

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -5,7 +5,7 @@ class Instrument < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :title, :brand, :price, :model, presence: true
-  validates :decription, length: { maximum: 1000, too_long: "%{count} characters 
+  validates :description, length: { maximum: 1000, too_long: "%{count} characters 
     is the maximum allow" }
   validates :title, length: { maximum: 140, too_long: "%{count} characters is 
     the maximum allow" }
